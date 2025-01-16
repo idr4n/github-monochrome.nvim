b47e6e16bd61fbbed2830c9e28fb7153d289d053
Ivan Duran <idurandiaz@gmail.com>
2025-01-16 13:07:02 +0300

    Update snacks_picker highlights

--- lua/github-monochrome/groups/snacks_picker.lua
+++ lua/github-monochrome/groups/snacks_picker.lua
@@ -6,14 +6,14 @@ M.url = "https://github.com/folke/snacks.nvim/blob/main/docs/picker.md"
 function M.get(c)
   -- stylua: ignore
   return {
-    SnacksPickerInputTitle          = { fg = c.orange, bold = true, bg = c.bg_float },
-    SnacksPickerBoxTitle            = { fg = c.orange, bold = true, bg = c.bg_float },
-    SnacksPickerPreviewTitle        = { fg = c.orange, bold = true, bg = c.bg_float },
+    SnacksPickerInputTitle          = { fg = c.orange, bg = c.bg_float },
+    SnacksPickerBoxTitle            = { fg = c.orange, bg = c.bg_float },
+    SnacksPickerPreviewTitle        = { fg = c.orange, bg = c.bg_float },
     SnacksPickerListCursorLine      = { bg = c.bg_highlight },
     SnacksPickerPreviewCursorLine   = { bg = c.visual },
     SnacksPickerSelected            = { fg = c.magenta},
-    SnacksPickerDir                 = { fg = c.fg },
-    SnacksPickerFile                = { fg = c.fg, bold = true },
+    SnacksPickerDir                 = { fg = c.hint_fg },
+    SnacksPickerFile                = { fg = c.fg },
     SnacksPickerMatch               = { fg = c.magenta, bold = true },
   }
 end
