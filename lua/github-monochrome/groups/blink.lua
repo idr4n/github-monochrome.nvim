a3ebaed770749073e6d441fe374d176e7292fa6f
Ivan Duran <idurandiaz@gmail.com>
2025-01-23 13:22:38 +0300

    Update BlinkCmpLabel fg color

--- lua/github-monochrome/groups/blink.lua
+++ lua/github-monochrome/groups/blink.lua
@@ -6,11 +6,13 @@ M.url = "https://github.com/saghen/blink.cmp"
 function M.get(c)
   -- stylua: ignore
   return {
-    BlinkCmpMenu            = { fg = c.fg, bg = c.bg_float },
-    BlinkCmpMenuBorder      = "FloatBorder",
-    BlinkCmpDoc             = { fg = c.fg, bg = c.bg_float },
-    BlinkCmpDocBorder       = "FloatBorder",
-    BlinkCmpMenuSelection   = { bg = c.bg_highlight },
+    BlinkCmpMenu               = { fg = c.fg, bg = c.bg_float },
+    BlinkCmpMenuBorder         = "FloatBorder",
+    BlinkCmpDoc                = { fg = c.fg, bg = c.bg_float },
+    BlinkCmpDocBorder          = "FloatBorder",
+    BlinkCmpMenuSelection      = { bg = c.bg_highlight },
+    BlinkCmpLabelDetail        = { fg = c.comment },
+    BlinkCmpLabelDescription   = { fg = c.comment },
   }
 end
 
