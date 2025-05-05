6f0eff6d3d21366455aeab6a6163d46b3b08a551
Ivan Duran <idurandiaz@gmail.com>
2025-05-05 11:26:39 +0300

    chore: update BufferLineIndicatorSelected fg color

--- lua/github-monochrome/groups/bufferline.lua
+++ lua/github-monochrome/groups/bufferline.lua
@@ -5,7 +5,7 @@ M.url = "https://github.com/akinsho/bufferline.nvim"
 ---@type gm.HighlightsFn
 function M.get(c, opts)
   return {
-    BufferLineIndicatorSelected = { fg = c.yellow, bg = opts.transparent and c.none or c.bg },
+    BufferLineIndicatorSelected = { fg = c.fg, bg = opts.transparent and c.none or c.bg },
   }
 end
 
