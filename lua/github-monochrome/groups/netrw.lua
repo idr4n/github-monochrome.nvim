d9885008f7ba2436b296a00ca9868e34c0c91dca
Ivan Duran <idurandiaz@gmail.com>
2025-07-02 23:24:49 +0300

    feat: add special syntax netrw hl groups

--- lua/github-monochrome/groups/netrw.lua
+++ lua/github-monochrome/groups/netrw.lua
@@ -2,6 +2,8 @@ local Util = require("github-monochrome.util")
 
 local M = {}
 
+vim.g.netrw_special_syntax = true
+
 ---@type gm.HighlightsFn
 function M.get(c)
   -- stylua: ignore
@@ -9,12 +11,22 @@ function M.get(c)
     netrwDir          = { fg = c.blue, bold = true },
     netrwClassify     = { fg = c.blue, bold = true }, -- end characters like / and @
     netrwSymLink      = { fg = c.orange, bold = true },
+    netrwLink         = { fg = c.blue, bold = true },
     netrwExe          = { fg = c.red },
     netrwTreeBar      = { fg = c.magenta },
-    netrwCompress     = { fg = c.cyan },
-    netrwBak          = { fg = c.blue },
     netrwSpecial      = { fg = c.orange },
     netrwMarkFile     = { bg = Util.blend_bg(c.purple, 0.2), fg = c.purple, bold = true },
+    -- Special syntax
+    netrwBak          = { fg = c.blue },
+    netrwCompress     = { fg = c.cyan },
+    netrwData         = { fg = c.red },
+    netrwDoc          = { fg = c.green },
+    netrwHdr          = { fg = c.cyan },
+    netrwLib          = { fg = c.cyan },
+    netrwMakefile     = { fg = c.red },
+    netrwObj          = { fg = c.yellow },
+    netrwPix          = { fg = c.orange },
+    netrwTmp          = { fg = c.fg_dark },
   }
 end
 
